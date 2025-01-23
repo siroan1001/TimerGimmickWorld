f08ce89197b6e657c626dfb218af1cda0d7a28c4
岩田紘輝 <2002gg1001@gmail.com>
2025-01-23 19:22:30 +0900

    TMP撤廃、レガジーTextに置き換え、オンオフ切り替え実装

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -10,9 +10,12 @@ public class Timer : UdonSharpBehaviour
     [UdonSynced] private float time;
     [UdonSynced] private bool state;
 
-    [SerializeField] private TimerStateStr statestr;
-    [SerializeField] private Transform timestr;
-    [SerializeField] private TimerErrorStr errorstr;
+    //[SerializeField] private TimerStateStr statestr;
+    //[SerializeField] private Transform timestr;
+    //[SerializeField] private TimerErrorStr errorstr;
+
+    //[SerializeField] private TimerStateStrDemo statestr;
+    //[SerializeField] private TimerTimeStrDemo timestr;
 
     AudioSource audioSource;
     [SerializeField] private AudioClip SE;
@@ -22,33 +25,35 @@ public class Timer : UdonSharpBehaviour
     {
         time = 0.0f;
         state = false;
-        statestr.SetStr(state);
+        //statestr.SetStr(state);
         audioSource = GetComponent<AudioSource>();
         MuteFlag = false;
     }
 
     private void Update()
     {
-        Debug.Log("TimerUpdate" + Networking.LocalPlayer.playerId);
-        if (state)
-        {
-            time -= Time.deltaTime;
-            if (time <= 0.0f)
-            {//タイマー終了
-                time = 0.0f;
-                if (!MuteFlag) audioSource.PlayOneShot(SE);
-                state = false;
-            }
+        //Debug.Log("TimerUpdate" + Networking.LocalPlayer.playerId);
+        //if (state)
+        //{
+        //    time -= Time.deltaTime;
+        //    if (time <= 0.0f)
+        //    {//タイマー終了
+        //        time = 0.0f;
+        //        if (!MuteFlag) audioSource.PlayOneShot(SE);
+        //        state = false;
+        //    }
 
-            RequestSerialization();
-        }
+        //    RequestSerialization();
+        //}
 
-        statestr.SetStr(state);
-        for (int i = 0; i < timestr.childCount; i++)
-        {
-            timestr.GetChild(i).GetComponent<TimerTimeStr>().SetStr(time);
-        }
+        //statestr.SetStr(state);
+        //for (int i = 0; i < timestr.childCount; i++)
+        //{
+        //    timestr.GetChild(i).GetComponent<TimerTimeStr>().SetStr(time);
+        //}
         //SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(UpdateAllPlayer));
+
+        //RequestSerialization();
     }
 
     //public void UpdateAllPlayer()
@@ -78,7 +83,7 @@ public class Timer : UdonSharpBehaviour
         Debug.Log("TimerAddTime" + Networking.LocalPlayer.playerId);
         if (state)
         {
-            errorstr.SetAlpha();
+            //errorstr.SetAlpha();
             return;
         }
 
@@ -97,7 +102,7 @@ public class Timer : UdonSharpBehaviour
     {
         if (state)
         {
-            errorstr.SetAlpha();
+            //errorstr.SetAlpha();
             return;
         }
 
@@ -112,7 +117,7 @@ public class Timer : UdonSharpBehaviour
 
     public void OnOff()
     {
-        if (time <= 0.0f) return;
+        //if (time <= 0.0f) return;
 
         if (!Networking.IsOwner(Networking.LocalPlayer, this.gameObject))
         {
@@ -120,6 +125,7 @@ public class Timer : UdonSharpBehaviour
         }
 
         state ^= true;
+        Debug.Log("オンオフ切り替え");
         RequestSerialization();
     }
 
@@ -159,5 +165,10 @@ public class Timer : UdonSharpBehaviour
         {
             Networking.SetOwner(Networking.LocalPlayer, children[i].gameObject);
         }
+    }
+
+    public bool State
+    {
+        get { return state; }
     }
 }
---
+++ Assets/Script/TimerStateStrDemo.cs
@@ -0,0 +1,41 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TimerStateStrDemo : UdonSharpBehaviour
+{
+    [SerializeField] private Timer script;
+
+    /*[UdonSynced] */private Text text;
+    private bool Flag;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+        Flag = script.State;
+        UpdateText();
+    }
+
+    private void Update()
+    {
+        Debug.Log("ステート文字の更新");
+        bool foge = script.State;
+        if (foge != Flag)
+        {
+            Flag = script.State;
+            UpdateText();
+        }
+    }
+
+    private void UpdateText()
+    {
+        if (Flag)   text.text = "On";
+        else        text.text = "Off";
+
+        //RequestSerialization();
+    }
+}
